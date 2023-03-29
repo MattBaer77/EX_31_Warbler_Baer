4a0fce4ca7dfddb5f03ade9105a6710f0732efad
Matthew Baer <matt.baer77@gmail.com>
2023-03-28 21:33:37 -0400

    modified secret key and removed defaults

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from secrets import sneakybeaky
 
 from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
@@ -19,7 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = sneakybeaky
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
