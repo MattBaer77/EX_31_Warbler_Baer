dafb71e06ffaf79a2785dabd8a23f87a892aa53f
Matthew Baer <matt.baer77@gmail.com>
2023-04-01 18:18:02 -0400

    Step Five: Profile Edit - POST route with validation

--- app.py
+++ app.py
@@ -216,13 +216,31 @@ def stop_following(follow_id):
 def profile():
     """Update profile for current user."""
 
-    # VALIDATE ON SUBMIT
+    # VALIDATE ON SUBMIT FOR POST ROUTE
 
     if not g.user:
         flash("Access unauthorized.", "danger")
         return redirect("/")
 
     form = UserEditForm(obj=g.user)
+
+    if form.validate_on_submit():
+        user = User.authenticate(g.user.username,
+                                form.password.data)
+
+        if user:
+            user.username = form.username.data
+            user.email = form.email.data
+            user.image_url = form.image_url.data
+            user.header_image_url = form.header_image_url.data
+            user.bio = form.bio.data
+
+            db.session.commit()
+            return redirect("/")
+
+        else:
+            flash("Access unauthorized.", "danger")
+            return render_template('/users/edit.html', form=form)
 
     return render_template('/users/edit.html', form=form)
 
