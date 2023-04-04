4f17b515f22b66a328f8372e524a44b396c41951
Matthew Baer <matt.baer77@gmail.com>
2023-04-04 01:35:19 -0400

    Added signup route tests to test_user_views.py

--- test_user_views.py
+++ test_user_views.py
@@ -15,7 +15,7 @@ from models import db, connect_db, Message, User
 # before we import our app, since that will have already
 # connected to the database
 
-os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
 
 
 # Now we can import app
@@ -31,3 +31,99 @@ db.create_all()
 # Don't have WTForms use CSRF at all, since it's a pain to test
 
 app.config['WTF_CSRF_ENABLED'] = False
+
+
+class UserViewTestCase(TestCase):
+    """Test views for users."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+
+        User.query.delete()
+        Message.query.delete()
+
+        self.client = app.test_client()
+
+        self.testuser = User.signup(username="testuser",
+                                    email="test@test.com",
+                                    password="testuser",
+                                    image_url=None)
+
+        self.testuser.id = 1
+
+        message1 = Message(id=1, text="test_message_views_text_1")
+        message2 = Message(id=2, text="test_message_views_text_2")
+
+        self.testuser.messages.append(message1)
+        self.testuser.messages.append(message2)
+
+        db.session.commit()
+
+        self.message1 = message1
+        self.message2 = message2
+
+        # Why is it making me do this? - Detached instance error if not. - Does not occur in other files.
+        self.message1.id
+        # Why is it making me do this? - Detached instance error if not. - Does not occur in other files.
+
+    def tearDown(self):
+        """Clean up any fouled transaction."""
+        
+        db.session.rollback()
+
+    def test_user_signup_get(self):
+        """Can a user view sign up page"""
+
+        with self.client as c:
+
+            resp = c.get("/signup")
+
+            self.assertEqual(resp.status_code, 200)
+            html = resp.get_data(as_text=True)
+            self.assertIn('<button class="btn btn-primary btn-lg btn-block">Sign me up!</button>', html)
+
+    def test_user_signup_post(self):
+        """Can a user sign up"""
+
+        with self.client as c:
+
+            resp = c.post("/signup", data={"username" : "testuser2", "email" : "test2@test.com", "password" : "testuser2", "image_url" : "testuser2.jpeg"})
+
+            self.assertEqual(resp.status_code, 302)
+            html = resp.get_data(as_text=True)
+            self.assertIn('<a href="/">/</a>', html)
+
+    def test_user_signup_post_redirect(self):
+        """Is a user appropriately redirected if successful signup"""
+
+        with self.client as c:
+
+            resp = c.post("/signup", data={"username" : "testuser2", "email" : "test2@test.com", "password" : "testuser2", "image_url" : "testuser2.jpeg"}, follow_redirects=True)
+
+            self.assertEqual(resp.status_code, 200)
+            html = resp.get_data(as_text=True)
+            self.assertIn('testuser2', html)
+            self.assertIn('testuser2.jpeg', html)
+
+    def test_user_signup_post_duplicate_username_redirect(self):
+        """Is a user appropriately redirected if using duplicate username"""
+
+        with self.client as c:
+
+            resp = c.post("/signup", data={"username" : "testuser", "email" : "test2@test.com", "password" : "testuser2", "image_url" : "testuser2.jpeg"}, follow_redirects=True)
+
+            self.assertEqual(resp.status_code, 200)
+            html = resp.get_data(as_text=True)
+            self.assertIn('Username or email already taken', html)
+
+    def test_user_signup_post_duplicate_email_redirect(self):
+        """Is a user appropriately redirected if using duplicate email"""
+
+        with self.client as c:
+
+            resp = c.post("/signup", data={"username" : "testuser2", "email" : "test@test.com", "password" : "testuser2", "image_url" : "testuser2.jpeg"}, follow_redirects=True)
+
+            self.assertEqual(resp.status_code, 200)
+            html = resp.get_data(as_text=True)
+            self.assertIn('Username or email already taken', html)
+
